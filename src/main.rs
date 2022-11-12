cb38229e9a94ebe25ff2b924e521945aa0e16f85
Quit975 <plushquotient@gmail.com>
2022-11-13 00:48:04 +0100

    First iteration for turn based gameplay

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod camera;
 mod components;
 mod spawner;
 mod systems;
+mod turn_state;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -20,6 +21,7 @@ mod prelude {
     pub use crate::components::*;
     pub use crate::spawner::*;
     pub use crate::systems::*;
+    pub use crate::turn_state::*;
 }
 
 use prelude::*;
@@ -27,7 +29,9 @@ use prelude::*;
 struct State {
     ecs : World,
     resources : Resources,
-    systems : Schedule
+    input_systems : Schedule,
+    player_systems : Schedule,
+    enemy_systems : Schedule
 }
 
 impl State {
@@ -45,11 +49,14 @@ impl State {
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
+        resources.insert(TurnState::AwaitingInput);
 
         Self {
             ecs,
             resources,
-            systems : build_scheduler()
+            input_systems : build_input_scheduler(),
+            player_systems : build_player_scheduler(),
+            enemy_systems : build_enemy_scheduler(),
         }
     }
 }
@@ -62,7 +69,20 @@ impl GameState for State {
         ctx.cls();
 
         self.resources.insert(ctx.key);
-        self.systems.execute(&mut self.ecs, &mut self.resources);
+        
+        let current_turn_state = self.resources.get::<TurnState>().unwrap().clone();
+        match current_turn_state {
+            TurnState::AwaitingInput => {
+                self.input_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+            TurnState::PlayerTurn => {
+                self.player_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+            TurnState::EnemyTurn => {
+                self.enemy_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+        }
+
         render_draw_buffer(ctx).expect("Render error!");
     }
 }
